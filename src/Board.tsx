e7552d0a57fb17ed0d2fb635ca1b91383e112f2a
Jason Lengstorf <jason@lengstorf.com>
2024-08-01 10:59:19 -0700

    feat: code built during episode

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,12 +1,57 @@
 import { BoardProps } from 'boardgame.io/react';
-import { CheckersState } from './Game';
+import { CheckersState, Color, Piece } from './Game';
+import { useState } from 'react';
 
 interface CheckersProps extends BoardProps<CheckersState> {}
 
 export const Board = ({ G, ctx, moves, playerID }: CheckersProps) => {
+  const [selectedCell, setSelectedCell] = useState<null | number>(null);
+  const [selectedRow, setSelectedRow] = useState<null | number>(null);
+
   return (
-    <div>
-      <h1>Checkers!</h1>
+    <div className="board">
+      {G.grid.map((row, rowIdx) => {
+        return (
+          <div className="row" key={`row-${rowIdx}`}>
+            {row.map((cell, cellIdx) => (
+              <div
+                key={`cell-${cellIdx}`}
+                className={`cell ${
+                  cell.color === Color.Black ? 'black' : 'white'
+                }`}
+                onClick={() => {
+                  if (cell.piece) {
+                    return;
+                  }
+
+                  moves.movePiece(selectedCell, selectedRow, cellIdx, rowIdx);
+                  setSelectedCell(null);
+                  setSelectedRow(null);
+                }}
+              >
+                {cell.piece ? (
+                  <div
+                    className={`piece ${
+                      cell.piece.color === Color.Black ? 'black' : 'white'
+                    } ${
+                      rowIdx === selectedRow && cellIdx === selectedCell
+                        ? 'selected'
+                        : ''
+                    }`}
+                    onClick={() => {
+                      setSelectedRow(rowIdx);
+                      setSelectedCell(cellIdx);
+                    }}
+                  >
+                    {rowIdx}
+                    {cellIdx}
+                  </div>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,246 @@
 import { Game } from 'boardgame.io';
+import { INVALID_MOVE } from 'boardgame.io/core';
 
-export interface CheckersState {
-  cells: (null | string)[];
+export enum Color {
+  Black,
+  White,
 }
 
-export const Checkers: Game<CheckersState> = {};
+export interface Piece {
+  color: Color;
+  isKing: boolean;
+}
+
+interface Cell {
+  color: Color;
+  piece: Piece | null;
+}
+
+export interface CheckersState {
+  grid: Array<Cell[]>;
+}
+
+export const Checkers: Game<CheckersState> = {
+  moves: {
+    movePiece: ({ G, ctx }, selectedCell, selectedRow, destCell, destRow) => {
+      // TODO: validate move!
+      // - does the current player own the piece in question?
+      // - is the move diagonal?
+      // - is it one square in the right direction?
+      // - is the destination empty?
+      const piece = G.grid[selectedRow][selectedCell].piece;
+
+      const allowedPieceColor =
+        ctx.currentPlayer === '0' ? Color.Black : Color.White;
+
+      if (piece?.color !== allowedPieceColor) {
+        return INVALID_MOVE;
+      }
+
+      const validRow =
+        piece?.color === Color.Black ? selectedRow + 1 : selectedRow - 1;
+      const validCells = [selectedCell - 1, selectedCell + 1].filter(
+        (v) => v >= 0 && v <= 7
+      );
+
+      if (destRow !== validRow) {
+        return INVALID_MOVE;
+      }
+
+      if (!validCells.includes(destCell)) {
+        return INVALID_MOVE;
+      }
+
+      G.grid[selectedRow][selectedCell].piece = null;
+      G.grid[destRow][destCell].piece = piece;
+    },
+  },
+
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
+  },
+
+  setup() {
+    return {
+      grid: [
+        [
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+        ],
+        [
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+        ],
+        [
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.Black, isKing: false },
+          },
+          { color: Color.White, piece: null },
+        ],
+        [
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+        ],
+        [
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+          { color: Color.Black, piece: null },
+          { color: Color.White, piece: null },
+        ],
+        [
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+        ],
+        [
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          {
+            color: Color.White,
+            piece: null,
+          },
+        ],
+        [
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+          { color: Color.White, piece: null },
+          {
+            color: Color.Black,
+            piece: { color: Color.White, isKing: false },
+          },
+        ],
+      ],
+    };
+  },
+};
